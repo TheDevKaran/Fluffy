4177c437e3926a5db45ac325520250debfd7593d
Dev Karan Yadav <devkaranyadavmilli@gmail.com>
2023-11-22 10:50:30 +0530

    First

---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:fluffy/home_page.dart';
+import 'package:fluffy/pallete.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Fluffy',
+      theme: ThemeData.light(useMaterial3: true).copyWith(
+        scaffoldBackgroundColor: Pallete.whiteColor,
+        appBarTheme: const AppBarTheme(
+          backgroundColor: Pallete.whiteColor,
+        ),
+      ),
+      home: const HomePage(),
+    );
+  }
+}
---
+++ lib/secret_keys.dart
@@ -0,0 +1,3 @@
+//secret_keys
+
+const openaiAPIkey = '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';
